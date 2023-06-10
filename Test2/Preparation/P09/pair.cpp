799d67fbeab2dab343a54861a5e41f2deb70a362
joao <up202106996@up.pt>
2023-06-10 11:12:54 +0100

    Finished P09

---
+++ Test2/Preparation/P09/pair.cpp
@@ -0,0 +1,80 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template<typename T1, typename T2>
+class Pair{
+    public:
+        Pair(T1 first, T2 second);
+        T1 get_first() const;
+        T2 get_second() const;
+        void show() const;
+    private:
+        T1 first_;
+        T2 second_;
+};
+
+template<typename T1, typename T2>
+Pair<T1,T2>::Pair(T1 first, T2 second) : first_(first), second_(second) {}
+
+template<typename T1, typename T2>
+T1 Pair<T1, T2>::get_first() const{return first_;}
+
+template<typename T1, typename T2>
+T2 Pair<T1, T2>::get_second() const{return second_;}
+
+template<typename T1, typename T2>
+void Pair<T1, T2>::show() const{
+    cout << '{' << first_ << ',' << second_ << '}';
+}
+
+bool compare_first(Pair<string, int> first, Pair<string, int> second){
+    return first.get_first() < second.get_first();
+}
+
+bool compare_second(Pair<string, int> first, Pair<string, int> second){
+    return first.get_first() < second.get_first();
+}
+
+void sort_by_first(vector<Pair<string, int>> vec){
+    sort(vec.begin(), vec.end(), compare_first);
+}
+
+void sort_by_second(vector<Pair<string, int>> vec){
+    sort(vec.begin(), vec.end(), compare_second);
+}
+
+void show(vector<Pair<string, int>> vec){
+    cout << '{';
+    for(auto v: vec){
+        v.show();
+    }
+    cout << '}';
+}
+
+int main(){
+    { vector<Pair<string, int>> persons = { {"Maria",17},{"Ana",21},{"Pedro",19} };
+    sort_by_first(persons);
+    show(persons); cout << '\n'; }
+    //{{Ana,21}{Maria,17}{Pedro,19}}
+    { vector<Pair<string, int>> persons = { {"Ana",19},{"Rui",16} };
+    sort_by_second(persons);
+    show(persons); cout << '\n'; }
+    //{{Rui,16}{Ana,19}}
+    { vector<Pair<string, int>> teams = { {"Porto",91},{"Benfica",74},{"Sporting",85} };
+    sort_by_first(teams);
+    show(teams); cout << '\n'; }
+    //{{Benfica,74}{Porto,91}{Sporting,85}}
+    { vector<Pair<string, int>> teams = { {"Porto",91},{"Benfica",74},{"Sporting",85} };
+    sort_by_second(teams);
+    show(teams); cout << '\n'; }
+    //{{Benfica,74}{Sporting,85}{Porto,91}}
+    { vector<Pair<string, int>> calories = { {"orange",37},{"egg",146},{"apple",56},{"yogurt",51} };
+    sort_by_second(calories);
+    show(calories); cout << '\n'; }
+    //{{orange,37}{yogurt,51}{apple,56}{egg,146}}
+    return 0;
+}
